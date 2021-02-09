923b5f8d06d3f0881fbc69d82141e4811870a19a
ShahEftakher <shaheftakher@iut-dhaka.edu>
2021-02-09 14:47:07 +0600

    Complete App

--- todo.js
+++ todo.js
@@ -1,13 +1,18 @@
-function displayList() {
+var allTasks = [];
+
+function getTask() {
   let input = document.getElementById("list");
+  console.log(input.value);
   saveTask(input.value);
   input.value = "";
 }
 
 function saveTask(input) {
   var keygen = Math.floor(Math.random() * 1000);
-  var taskObj = { key: keygen, task: input };
-  window.localStorage.setItem(keygen, JSON.stringify(taskObj));
+  var taskObj = { key: keygen, task: input, isComplete: false };
+  console.log(typeof(allTasks));
+  allTasks.push(taskObj);
+  window.localStorage.setItem("task", JSON.stringify(allTasks));
   addElements(taskObj);
 }
 
@@ -19,46 +24,63 @@ function addElements(input) {
     var inputTask = document.createElement("input");
     inputTask.type = "text";
     inputTask.id = "task";
-    paragraph.innerHTML = input.task;
-    inputTask.disabled = true;
+    var currentTask = input.task;
+    if (input.isComplete) {
+      paragraph.innerHTML = currentTask.strike();
+    } else {
+      paragraph.innerHTML = currentTask;
+    }
     var editBtn = document.createElement("button");
-    editBtn.textContent = "update";
+    editBtn.textContent = "Edit";
     editBtn.onclick = function () {
-      if (editBtn.textContent === "update") {
-        editBtn.textContent = "done";
+      if (editBtn.textContent === "Edit") {
+        form.appendChild(inputTask);
+        editBtn.textContent = "Update";
         inputTask.disabled = false;
         inputTask.value = input.task;
       } else {
-        editBtn.textContent = "update";
+        editBtn.textContent = "Edit";
         paragraph.innerHTML = inputTask.value;
         input.task = inputTask.value;
         inputTask.value = "";
-        inputTask.disabled = true;
         updateTask(input);
-        console.log(inputTask.value);
+        inputTask.remove();
       }
     };
     form.onsubmit = function () {
-        editBtn.textContent = "update";
-        paragraph.innerHTML = inputTask.value;
-        input.task = inputTask.value;
-        inputTask.value = "";
-        inputTask.disabled = true;
-        updateTask(input);
-        console.log(inputTask.value);
-      }
+      editBtn.textContent = "Edit";
+      paragraph.innerHTML = inputTask.value;
+      input.task = inputTask.value;
+      inputTask.value = "";
+      updateTask(input);
+      inputTask.remove();
+    };
     var completeBtn = document.createElement("button");
     completeBtn.textContent = "Completed";
     completeBtn.onclick = function () {
+      var strikeWord = input.task;
+      paragraph.innerHTML = strikeWord.strike();
+      input.isComplete = true;
+      updateTask(input);
+      editBtn.remove();
+      inputTask.remove();
+      console.log(strikeWord.strike());
+    };
+    var deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Remove";
+    deleteBtn.onclick = function () {
       div.remove();
-      localStorage.removeItem(input.key);
+      removeTask(input);
     };
 
-    form.appendChild(inputTask);
     div.appendChild(form);
     div.appendChild(paragraph);
-    div.appendChild(editBtn);
+    if (input.isComplete) {
+    } else {
+      div.appendChild(editBtn);
+    }
     div.appendChild(completeBtn);
+    div.appendChild(deleteBtn);
     div.appendChild(document.createElement("br"));
 
     return div;
@@ -67,14 +89,30 @@ function addElements(input) {
 }
 
 function updateTask(input) {
-  window.localStorage.setItem(input.key, JSON.stringify(input));
+  var currentIndex=allTasks.indexOf(input);
+  console.log(currentIndex);
+  console.log(input);
+  allTasks[currentIndex]=input;
+  window.localStorage.setItem("task", JSON.stringify(allTasks));
+}
+
+function removeTask(input) {
+  var currentIndex=allTasks.indexOf(input);
+  if (currentIndex > -1) {
+    allTasks.splice(currentIndex, 1);
+  }
+  window.localStorage.setItem("task", JSON.stringify(allTasks));
 }
 
 function getTaskFromStorage() {
-  var keys = Object.keys(localStorage);
-  keys.forEach((key) => {
-    var data = JSON.parse(window.localStorage.getItem(key));
-    addElements(data);
-  });
+  allTasks = JSON.parse(window.localStorage.getItem("task"));
+  if (allTasks === null) {
+    window.localStorage.setItem("task", JSON.stringify([]));
+    allTasks = JSON.parse(window.localStorage.getItem("task"));
+  } else {
+    allTasks.forEach((task) => {
+      addElements(task);
+    });
+  }
 }
 getTaskFromStorage();
